02b19dd604158fd51a918002de233f38997f84ec
Binsure5 <72709170+Binsure5@users.noreply.github.com>
2025-05-31 18:15:11 +0900

    Remove custom domain configuration

--- next.config.js
+++ next.config.js
@@ -4,9 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // 커스텀 도메인을 사용할 때는 basePath와 assetPrefix가 필요하지 않습니다
-  // basePath: '/LumexWeb',
-  // assetPrefix: '/LumexWeb/',
+  basePath: '/LumexWeb',
+  assetPrefix: '/LumexWeb/',
   trailingSlash: true,
 }
 
